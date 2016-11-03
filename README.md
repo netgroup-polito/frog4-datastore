292ed26081699434d0c0a29e5393cb3e874b1c2f
Luigi <s222330@studenti.polito.it>
2016-11-03 17:01:16 +0100

    Added dependencies to manage chunked uploads

--- README.md
+++ README.md
@@ -26,7 +26,7 @@ Creating a private environment in which we run Django 1.8; unfortunately API cha
 
 	virtualenv .env
 	source .env/bin/activate
-	pip install django==1.8.2 djangorestframework MySQL-python django-rest-swagger==0.3.5 wrapt bcrypt
-	python manage.py makemigrations
+	pip install django==1.8.2 djangorestframework MySQL-python django-rest-swagger==0.3.5 django-chunked-upload django-cors-headers wrapt bcrypt
+	python manage.py makemigrations VNF
 	python manage.py migrate
 	python manage.py runserver --d VNF_ServiceConfig.ini [uses config/default-config.ini if '--d' is missing]
--- VNFRepository/settings.py
+++ VNFRepository/settings.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import datetime
+from corsheaders.defaults import default_headers
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -37,10 +39,13 @@ INSTALLED_APPS = (
 	'rest_framework',
 	'VNF', 
 	'rest_framework_swagger',
+    'chunked_upload',
+    'corsheaders',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -106,3 +111,12 @@ STATIC_ROOT = os.path.join(os.path.abspath('.'), 'static')
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_HEADERS = default_headers + (
+    'content-range',
+    'content-disposition',
+)
+
+CHUNKED_UPLOAD_PATH = ('chunked_uploads')
+CHUNKED_UPLOAD_EXPIRATION_DELTA = datetime.timedelta(hours=1)
+CHUNKED_UPLOAD_ABSTRACT_MODEL = True
