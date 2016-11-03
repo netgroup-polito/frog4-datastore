fd6054d8477814e38c439f1b508ab97750cf642f
Luigi <s222330@studenti.polito.it>
2016-11-03 16:54:29 +0100

    Bug Fix: Wrong variable name

--- VNF/imageRepository/LocalRepository.py
+++ VNF/imageRepository/LocalRepository.py
@@ -7,12 +7,12 @@ class LocalRepository(object):
 		self.imageDir = imageDir
 
 	def storeImage(self, vnf_id, imageFile):
-		with open(self.imagesDir+str(vnf_id),'wb')as f:
+		with open(self.imageDir + str(vnf_id), 'wb')as f:
                 	for chunk in imageFile.chunks():
                         	f.write(chunk)
 
 	def getImage(self, vnf_id):
-		filename = self.imagesDir+str(vnf_id)
+		filename = self.imageDir + str(vnf_id)
 
 		wrapper = FileWrapper(file(filename))
 		fileLen = os.path.getsize(filename)
