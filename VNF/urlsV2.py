105f74b2f675beaabfa56639c6eb1a1b54a578da
Roberto Bonafiglia <roberto.bonafiglia@gmail.com>
2016-11-28 14:38:43 +0100

    Update URL

--- VNF/urlsV2.py
+++ VNF/urlsV2.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 from VNF import views
 
 urlpatterns = [
-        url(r'^/vnf-template/$', views.VNFTemplateAll.as_view(), name='VNF Template'),
-        url(r'^/vnf-template/(?P<vnf_id>[^/]+)/$', views.VNFTemplate.as_view(), name='VNF List'),
+        url(r'^/nf_template/$', views.VNFTemplateAll.as_view(), name='VNF Template'),
+        url(r'^/nf_template/(?P<vnf_id>[^/]+)/$', views.VNFTemplate.as_view(), name='VNF List'),
         url(r'^/vnf-image/(?P<vnf_id>[^/]+)/$', views.VNFImage.as_view(), name='VNF image'),
         url(r'^/nffg/(?P<nffg_id>[^/]+)/$', views.NFFGTemplate.as_view(), name='NFFG data'),
         url(r'^/nffg/$', views.NFFGAll.as_view(), name='NFFG list'),
-	url(r'^/vnf-capability/(?P<capability>[^/]+)/$', views.Capability.as_view(), name='Capability')
+	url(r'^/nf_capability/(?P<capability>[^/]+)/$', views.Capability.as_view(), name='Capability')
 ]
