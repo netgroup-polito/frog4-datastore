21d3e28853af3d6dfe905178b62d4dd756bde22b
Luigi <s222330@studenti.polito.it>
2017-02-02 17:38:53 +0100

    Support for storing image files with multiple extensions (e.g. tar.gz) + fixes

--- datastore/imageRepository/LocalRepository.py
+++ datastore/imageRepository/LocalRepository.py
@@ -8,9 +8,18 @@ class LocalRepository(object):
         self.imagesDir = imagesDir
 
     def storeImage(self, vnf_id, imageFile):
-        ext = os.path.splitext(imageFile.name)[1]
+        # Getting original file extension
+        ext = os.extsep + imageFile.name.split(os.extsep, 1)[1] if len(imageFile.name.split(os.extsep, 1)) == 2 else ""
+        # Create the images folder if it doesn't exists
         if not os.path.exists(self.imagesDir):
             os.makedirs(self.imagesDir)
+        # Remove the old image if it exists. This is necessary because in case of an update, if the new image has a
+        # different extension, the old file will not be overwritten.
+        try:
+            self.deleteImage(vnf_id)
+        except OSError:
+            pass
+        # Store the new uploaded image file
         with open(os.path.join(self.imagesDir, str(vnf_id) + ext), 'wb') as f:
             for chunk in imageFile.chunks():
                 f.write(chunk)
@@ -22,5 +31,7 @@ class LocalRepository(object):
         return (wrapper, fileLen)
 
     def deleteImage(self, vnf_id):
-        filename = glob.glob(os.path.join(self.imagesDir, str(vnf_id) + '.*'))[0]
+        filename = glob.glob(os.path.join(self.imagesDir, str(vnf_id) + '.*'))[0] if len(
+            glob.glob(os.path.join(self.imagesDir, str(vnf_id) + '.*'))) != 0 else os.path.join(self.imagesDir,
+                                                                                                str(vnf_id))
         os.remove(filename)
