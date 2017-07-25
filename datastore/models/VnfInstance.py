0bc37ce4a93dc5375bfea5e343f511166af6c840
DavideXX92 <davithepezz@hotmail.it>
2017-07-25 12:14:33 +0200

    foreign_key constraint modified

--- datastore/models/VnfInstance.py
+++ datastore/models/VnfInstance.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class VNF_Instance(models.Model):
     vnf_instance_id = models.CharField(primary_key=True, unique=True, max_length=100)
-    bootstrap_configuration = models.ForeignKey('NF_Configuration', on_delete=models.CASCADE, blank=False, null=True)
+    bootstrap_configuration = models.ForeignKey('NF_Configuration', blank=False, null=True)
     restEndpoint = models.CharField(max_length=1000, blank=True)
 
 # All 'Model' has to be added to __init__.py file. Check such file fore further information
